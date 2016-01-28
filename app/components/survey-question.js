bba3f213a3a61afd3d35d6fc0a2c28afa99f5c1b
Lauren Barker <leb2dg@virginia.edu>
2016-01-28 16:01:48 -0500

    add question type selector to survey question

--- app/components/survey-question.js
+++ app/components/survey-question.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	questionType: 'free response',
+	questionTypes: [ 'free response', 
+		'fill in the blank', 
+		'multiple choice',
+	  	'select all that apply',
+	  	'matching',
+	],
+
+	actions: {
+	    selectQuestionType: function(questionType) {
+	      this.set('questionType', questionType);
+	    },
+	}
 });
