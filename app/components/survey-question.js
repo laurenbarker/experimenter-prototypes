e579f5948b97c7972f6c19d8b8487b8bed3d706a
Lauren Barker <leb2dg@virginia.edu>
2016-01-28 08:58:46 -0500

    add support for dynamic addition of questions

--- app/components/survey-question.js
+++ app/components/survey-question.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  numberOfQuestions: 1,
+  
+  rangeOfQuestions: Ember.computed('numberOfQuestions', function() {
+    var numberOfQuestions = this.get('numberOfQuestions');
+    return Array
+      .apply(null, Array(numberOfQuestions))
+      .map(function (_, i) {return i;});
+  }),
+  
+  actions: {
+    add: function(){   
+       this.incrementProperty('numberOfQuestions');
+    }
+  }// actions
 });
